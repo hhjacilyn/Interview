2039d36e18a656adaac89b80ecb2a92e6ddf79b9
hhjacilyn <lijaci@seas.upenn.edu>
2025-01-13 16:58:08 -0500

    New NoSQL

---
+++ NoSQL_2.py
@@ -0,0 +1,49 @@
+import json
+import os
+from datetime import datetime
+from pymongo import MongoClient
+
+
+# Step 1: Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/") # default localhost
+db = client["financial_database"]  # create or connect to the database
+collection = db["financials"]  # create or connect to the collection
+
+# Step 2: Drop the collection if already exists
+collection.drop()
+
+# Step 3: Dynamically load today's JSON file into MongoDB
+folder = r"C:\Users\jacil\Desktop\Interview\Python"
+today = datetime.today().strftime('%Y%m%d')
+file_name = f"financials_{today}.json"
+
+file_path = os.path.join(folder, file_name)
+
+# check if the file exists
+if not os.path.exists(file_path):
+    print(f"Error: File '{file_path}' not found.")
+else:
+    with open(file_path, 'r') as file:
+        data = json.load(file)   # read the JSON file
+        collection.insert_many(data)   # inset the data into the MongoDB collection
+    print("Data successfully inserted into MongoDB!")
+
+
+# Step 4: Query data using pymongo
+print("\nALL records:")
+for record in collection.find():
+    print(record)
+
+
+print("\nBalances greater than $5000:")
+for record in collection.find({"balance": {"$gt": 5000}}):   # $gt: greater than
+    print(record)
+
+# _id: None - no grouping key is used, all records are treated as a single group
+pipeline = [{"$group": {"_id": None, "total_balance": {"$sum": "$balance"}}}]
+result = list(collection.aggregate(pipeline))  # convert the aggregation result into a list (list of dictionaries)
+print(f"\nTotal balance: {result[0]['total_balance']}")
+
+
+# Step 5: Close the connection
+client.close()
